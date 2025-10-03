8945951491e0bb9d3ff60767012cb4befc9e2c14
Udit8348 <usubramanya91@gmail.com>
2025-10-03 19:09:50 -0400

    gemm

---
+++ cuda/gemm.cu
@@ -0,0 +1,28 @@
+#include <cuda_runtime.h>
+
+__global__ void matrix_multiplication_kernel(const float* A, const float* B, float* C, int M, int N, int K) {
+    // localize thread
+    int row = blockDim.y * blockIdx.y + threadIdx.y;
+    int col = blockDim.x * blockIdx.x + threadIdx.x;
+
+    // a thead will compute its respective dot product for its location
+    if (row < M && col < K) {
+        float sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += A[i + row * N] * B[col + i * K];
+        }
+
+        C[row * K + col] = sum;
+    }
+    
+}
+
+// A, B, C are device pointers (i.e. pointers to memory on the GPU)
+extern "C" void solve(const float* A, const float* B, float* C, int M, int N, int K) {
+    dim3 threadsPerBlock(16, 16);
+    dim3 blocksPerGrid((K + threadsPerBlock.x - 1) / threadsPerBlock.x,
+                       (M + threadsPerBlock.y - 1) / threadsPerBlock.y);
+    
+    matrix_multiplication_kernel<<<blocksPerGrid, threadsPerBlock>>>(A, B, C, M, N, K);
+    cudaDeviceSynchronize();
+}
